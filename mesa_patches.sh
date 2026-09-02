7e331f8de5d53f22cf948bfc8ed8f29165c7df20
cgik <idkzzz50@gmail.com>
2026-09-02 16:12:35 +0700

    Create mesa_patches.sh

---
+++ mesa_patches.sh
@@ -0,0 +1,21 @@
+cd external/mesa
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/55/480155/4 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/43/489143/3 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/09/489409/1 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/84/489684/1 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/43/480143/2 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/44/480144/2 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/45/480145/2 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/46/480146/2 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/47/480147/2 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/48/480148/2 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/49/480149/2 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/50/480150/2 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/51/480151/3 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/52/480152/3 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/53/480153/3 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/54/480154/3 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/39/480139/2 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/40/480140/2 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/41/480141/2 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_mesa refs/changes/42/480142/2 && git checkout FETCH_HEAD
