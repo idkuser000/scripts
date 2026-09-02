40a52b91d049379bda17fffeef89aaeb2ae25a90
cgik <idkzzz50@gmail.com>
2026-09-02 18:24:31 +0700

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -1,3 +1,4 @@
+curl https://raw.githubusercontent.com/idkuser000/scripts/refs/heads/main/patches/0001-Set-default-refresh-rate-to-61Hz.patch | patch -p1
 curl https://raw.githubusercontent.com/idkuser000/scripts/refs/heads/main/patches/0001-libmeminfo-Firstly-check-whether-if-kBpfGpuMemTotalM.patch | patch -p1
 curl https://raw.githubusercontent.com/idkuser000/scripts/refs/heads/main/patches/8e4ced4.diff | patch -p1
 curl https://https://raw.githubusercontent.com/idkuser000/scripts/refs/heads/main/patches/a723c14.diff | patch -p1
