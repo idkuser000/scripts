f638f6d40d3529cb87a92ba1216c502d78b93d01
cgik <idkzzz50@gmail.com>
2026-09-02 11:06:58 +0700

    Create patches.sh

---
+++ patches.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/idkuser000/scripts/refs/heads/main/patches/0001-libmeminfo-Firstly-check-whether-if-kBpfGpuMemTotalM.patch | patch -p1
+curl https://raw.githubusercontent.com/idkuser000/scripts/refs/heads/main/patches/8e4ced4.diff | patch -p1
+curl https://https://raw.githubusercontent.com/idkuser000/scripts/refs/heads/main/patches/a723c14.diff | patch -p1
+curl https://raw.githubusercontent.com/idkuser000/scripts/refs/heads/main/patches/c422e0a.diff | patch -p1
