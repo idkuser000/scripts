d6b2f753926d9c2cd81205f85a3cbd4504633a54
cgik <idkzzz50@gmail.com>
2026-09-04 18:27:37 +0700

    Update dependencies_pixelos.sh

--- scripts/dependencies_pixelos.sh
+++ scripts/dependencies_pixelos.sh
@@ -8,9 +8,9 @@ git clone https://github.com/LineageOS/android_external_linux-firmware-mainline 
 git clone https://github.com/LineageOS/android_hardware_mainline_common --depth 1 -b lineage-24.0 hardware/mainline/common
 git clone https://github.com/LineageOS/android_hardware_mainline_qcom --depth 1 -b lineage-24.0 hardware/mainline/qcom
 git clone https://github.com/LineageOS/android_kernel_mainline_configs --depth 1 -b lineage-24.0 kernel/mainline/configs
-git clone https://github.com/LineageOS/android_device_mainline_common --depth 1 -b lineage-24.0 device/mainline/common
+git clone https://github.com/idkuser000/android_device_mainline_common --depth 1 -b seventeen device/mainline/common
 git clone https://github.com/LineageOS/android_external_mesa --depth 1 -b lineage-23.2 external/mesa
 git clone https://github.com/LineageOS/android_prebuilts_mesa-build-dep --depth 1 -b lineage-23.2 prebuilts/mesa-build-dep
-git clone https://github.com/LineageOS/android_device_mainline_generic --depth 1 -b lineage-24.0 device/mainline/generic
+git clone https://github.com/idkuser000/android_device_mainline_generic --depth 1 -b seventeen device/mainline/generic
 git clone https://github.com/LineageOS/android_external_tinyhal --depth 1 -b lineage-24.0 external/tinyhal
 git clone https://github.com/LineageOS/android_vendor_mainline --depth 1 -b lineage-24.0 vendor/mainline
