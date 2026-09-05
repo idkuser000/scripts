9a7f4d34f3fd8ba5a440111bd8fcc1c28e118cdd
cgik <idkzzz50@gmail.com>
2026-09-05 10:15:14 +0700

    Update lineage-a17.sh

--- lineage-a17.sh
+++ lineage-a17.sh
@@ -4,6 +4,10 @@
 
 rm -rf .repo/local_manifests
 rm -rf prebuilts/gcc
+rm -rf frameworks/base
+rm -rf system/memory/libmeminfo
+rm -rf external/debian-linux
+rm -rf kernel/mainline/android-mainline
 
 # init & syncing
 repo init -u https://github.com/LineageOS/android.git -b lineage-24.0 --git-lfs --depth=1
