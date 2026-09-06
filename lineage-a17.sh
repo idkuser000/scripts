83620c67f9fced035077a32755bdabd8f846b0ec
cgik <idkzzz50@gmail.com>
2026-09-06 10:02:15 +0700

    Update lineage-a17.sh

--- lineage-a17.sh
+++ lineage-a17.sh
@@ -44,6 +44,7 @@ lineage/scripts/repopick/repopick.py 471111 -f
 lineage/scripts/repopick/repopick.py 471112 -f
 lineage/scripts/repopick/repopick.py 471113 -f
 lineage/scripts/repopick/repopick.py 501163 -f
+lineage/scripts/repopick/repopick.py 501351 -f
 
 rm -rf external/mainline-hw-deps
 
