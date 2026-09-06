0b25893f8971bb812a8870a3ccecc0525fccafaf
cgik <idkzzz50@gmail.com>
2026-09-06 20:51:35 +0700

    Update lineage-a17.sh

--- lineage-a17.sh
+++ lineage-a17.sh
@@ -8,7 +8,6 @@ rm -rf frameworks/base
 rm -rf system/memory/libmeminfo
 rm -rf external/debian-linux
 rm -rf kernel/mainline/android-mainline
-rm -rf system/core
 rm -rf device/mainline/generic
 
 # init & syncing
@@ -24,9 +23,6 @@ export TZ="Asia/Ho_Chi_Minh"
 
 # clone dependencies
 rm -rf device/mainline/generic ; git clone https://github.com/idkuser000/android_device_mainline_generic --depth=1 -b lineage-24.0 device/mainline/generic
-
-git clone https://github.com/me-cafebabe-aosp-mainline/android_external_alsa-lib --depth=1 -b lineage-24.0 external/alsa-lib
-git clone https://github.com/me-cafebabe-aosp-mainline/android_external_alsa-ucm-conf --depth=1 -b lineage-24.0 external/alsa-ucm-conf
 
 lineage/scripts/repopick/repopick.py 492595 -f
 vendor/lineage/build/tools/roomservice.py generic true device/mainline/Generic_x86_64
